5f2e208343aae00727f7db06144e6f68375ea830
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-05 09:59:13 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const sendData = document.querySelector('[type="submit"]')
+const data = document.querySelector('.login-form')
 
+
+data.addEventListener("submit", qqq)
+
+function qqq(e) {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Заповни всі поля!");
+    }
+    console.log({ Email:email.value , Password:password.value });
+    e.currentTarget.reset();
+}
+
+
+
+
+
+ 
