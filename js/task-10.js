27bc628203cf619c98ffaa0c3de617de194e6664
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-07 08:44:16 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const quantity = document.querySelector("input")
+const divAdd = document.getElementById("boxes")
+const add = document.querySelector('[data-create]')
+const remove = document.querySelector('[data-destroy]')
+
+
+add.addEventListener("click", createBoxes)
+
+function createBoxes(amount) {
+  amount = quantity.valueAsNumber
+  divAdd.style.display = "flex"
+
+for (let i = 1; i <= amount; i++) {
+	let div = document.createElement('div');
+  divAdd.appendChild(div);
+  div.style.width = "30px"
+  div.style.height = "30px"
+  div.style.backgroundColor = getRandomHexColor()
+  
+}
+}
+
+
+remove.addEventListener("click", destroyBoxes)
+
+function destroyBoxes() {
+  divAdd.innerHTML = "";
+}
+
+
+
